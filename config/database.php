3d86f3b687571eb82fc09e035595d2d4cdb73cc0
Mariano <mariano.martinotti@pampago.site>
2026-04-03 11:25:42 -0300

    add & now

--- config/database.php
+++ config/database.php
@@ -7,17 +7,17 @@ class Database {
     private $user = "c2761701_pampadb" || "root";
     private $pass = "VE61foweba" || "";*/
     
-    /*
+    
     private $host = "localhost";
     private $db = "c2761701_pampadb";
     private $user = "c2761701_pampadb";
-    private $pass = "VE61foweba";*/
+    private $pass = "VE61foweba";
     
-    
+    /*
     private $host = "localhost";
     private $db   = "pampagodb";
     private $user =  "root";
-    private $pass = "";
+    private $pass = "";*/
     
     public function connect() {
         return new PDO(
